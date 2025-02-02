fed18092c0b17f6cd58cb6c38a6c14bcfb894d19
jubiij <robot_ruttunen@hotmail.com>
2025-02-02 18:05:37 +0200

    3.7 puhluettelo backend step7

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const app = express()
+const morgan = require('morgan')
 
 let persons = [
     {
@@ -25,18 +25,21 @@ let persons = [
      }
 ]
 
+app.use(express.json())
+app.use(morgan('tiny'))
 
-// get etusivu
+
+// GET etusivu
 app.get('/', (request, response) => {
     response.send('<h1>Puhelinluettelo backend</h1>')
 })
 
-// get all
+// GET all
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-// get by id
+// GET by id
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(person => person.id === id)
@@ -47,12 +50,58 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-    
 })
 
-// get info page
+// GET info page
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
+// DELETE
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+// POST
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    
+    // arvotaan luku 1-10000, joka annetaan id:nä
+    const generateId = Math.floor(Math.random() * 10000)
+    person.id = String(generateId)
+
+    // katsotaan onko nimi olemassa persons taulukossa
+    const nameExists = persons.find(person => person.name.toLowerCase() === request.body.name.toLowerCase())
+    
+    // jos nimi on tyhjä error:
+    if (!person.name) {
+        return response.status(400).json({
+            error: 'name is missing'
+        })     
+    }
+    // jos numero on tyhjä error:
+    if (!person.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    }
+    // jos nimi on jo olemassa error:
+    if(nameExists) {
+        return response.status(400).json({
+            error:'name must be inique'
+        })
+    }
+
+    // uusi taulukko jossa request.bodynä saatu henkilö
+    persons = persons.concat(person)
+    
+    console.log(generateId)
+    console.log(person)
+    
+    response.json(person)
 })
 
 const PORT = 3001
