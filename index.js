9aff3312a5b0c288ff7184fc5dd4ddfcec6a7293
jubiij <robot_ruttunen@hotmail.com>
2025-02-06 13:42:14 +0200

    eslint added and configuration done

--- index.js
+++ index.js
@@ -13,98 +13,98 @@ app.use(morgan('tiny'))
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Puhelinluettelo backend</h1>')
+  response.send('<h1>Puhelinluettelo backend</h1>')
 })
 
 // GET all
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 // GET by id
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-          if (person) {
-            response.json(person)
-          } else {
-            response.status(404).end()
-          }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 // GET info page
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${Person.length} people <br> ${new Date()}`)
+  response.send(`Phonebook has info for ${Person.length} people <br> ${new Date()}`)
 })
 
 // DELETE
 app.delete('/api/persons/:id', (request, response, next) => {
-   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 // POST
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    if (body.name === "" || body.number === "") {
-        return response.status(400).json({ error: 'name or number missing!'})
-    }
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  if (body.name === '' || body.number === '') {
+    return response.status(400).json({ error: 'name or number missing!' })
+  }
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
-    Person.findOneAndUpdate(
-      { name: body.name }, // etsitään mitä halutaan
-      { $set: { number: body.number } }, // päivitetään kenttä
-      { new: true } // Palautetaan päivitetty asiakirja
-    )
-      .then(updatedPerson => {
-        if (!updatedPerson) {
-          return response.status(404).json({ error: 'person not found' });
-        }
-        response.json(updatedPerson); // Lähetetään päivitetty asiakirja
-      })
-      .catch(error => next(error));
-  });
+  const body = request.body
+  Person.findOneAndUpdate(
+    { name: body.name }, // etsitään mitä halutaan
+    { $set: { number: body.number } }, // päivitetään kenttä
+    { new: true } // Palautetaan päivitetty asiakirja
+  )
+    .then(updatedPerson => {
+      if (!updatedPerson) {
+        return response.status(404).json({ error: 'person not found' })
+      }
+      response.json(updatedPerson) // Lähetetään päivitetty asiakirja
+    })
+    .catch(error => next(error))
+})
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  // olemattomien osoitteiden käsittely
-app.use(unknownEndpoint) 
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message})
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  // virheellisten pyyntöjen käsittely
-  app.use(errorHandler)
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3 ) {
-    console.log('give password as argument')
-    process.exit(1)
-} 
+  console.log('give password as argument')
+  process.exit(1)
+}
 
 const password = process.argv[2]
 
@@ -14,30 +14,30 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
+  name: process.argv[3],
+  number: process.argv[4]
 })
 
 if(process.argv.length === 3) {
-    console.log('Phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  console.log('Phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else {
-    person.save().then(result => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 
